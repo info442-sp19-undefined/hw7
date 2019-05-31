08906e34182cbedaf09a6fe5b52a24bacaaabd0e
VeronicaChang <vchang9@uw.edu>
2019-05-30 23:30:39 -0700

    add questions to display in modal

--- src/GameManager.js
+++ src/GameManager.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import firebase from "firebase/app";
 import './css/room.css';
 import {
-    FormGroup,
+    Input,
     InputGroup,
     InputGroupAddon,
-    Input,
     Label,
     Button,
     Col,
-    Row
+    Row,
+    Form
 } from "reactstrap";
+import { Redirect } from 'react-router-dom';
+import { Modal, ModalHeader, ModalFooter } from 'reactstrap';
 const uniqid = require("uniqid");
-
+const questionFile = require("./questions.json");
 export class GameManager extends Component {
     constructor(props) {
         super(props);
@@ -21,7 +23,8 @@ export class GameManager extends Component {
             uid: "",
             roomName: "",
             numQuestions: 5,
-            toggleAnalysis: false
+            toggleAnalysis: false,
+            created: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.onClick = this.onClick.bind(this);
@@ -44,6 +47,7 @@ export class GameManager extends Component {
                 showAnalysis: this.state.toggleAnalysis
             }
         });
+        this.setState({ created: true });
     }
 
     isValid(val, name) {
@@ -73,20 +77,57 @@ export class GameManager extends Component {
         });
     }
     render() {
-        let isEnabled = (this.state.fname !== "" || this.state.numQuestions !== 0);
+        let isEnabled = (this.state.fname !== "");
+        let button = null;
+        if (this.state.created) {
+            return <Redirect to={{
+                pathname: "/" + this.state.roomName + "/Categories/",
+                state: { maxQuestions: this.state.numQuestions }
+            }} />
+        } else {
+            button = (
+                <Button
+                    onClick={this.handleCreateRoom}
+                    disabled={!isEnabled}
+                    style={{
+                        fontSize: "18px",
+                        borderRadius: "20px",
+                        width: "200px",
+                        height: "2em",
+                        background: "#226597",
+                        color: "white",
+                        display: "flex",
+                        justifyContent: "center",
+                        padding: "0px",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        marginTop: "50px"
+                    }} >
+                    Create Room
+                </ Button>
+            )
+        }
         return (
-            <div className="Settings">
-                <h1 className="header">Settings</h1>
-                <FormGroup className="userName">
-                    <Label>Organizer Name </Label>
-                    <Input placeholder="First Name" name="fname" onChange={this.handleChange} id="fname" />
-                </FormGroup>
-                <FormGroup className="roomName">
-                    <Label>Custom Room Name</Label>
-                    <Input placeholder={this.state.roomName} name="roomName" onChange={this.handleChange} id="roomName" />
-                </FormGroup>
-                <div className="qNumber">
-                    <Label>Number of Icebreaker Questions</Label>
+            <div>
+                <Form id="newRoom-form">
+                    <h1 className="header">Settings</h1>
+                    <Label style={{ marginTop: "40px" }}> Organizer Name </Label>
+                    <Input
+                        placeholder="First Name"
+                        name="fname"
+                        onChange={this.handleChange}
+                    id="fname"
+                        style={{ width: "300px", borderRadius: "20px", paddingLeft: "24px" }}
+                    />
+                    <Label style={{ marginTop: "20px" }}>Custom Room Name</Label>
+                    <Input
+                        placeholder={this.state.roomName}
+                        name="roomName"
+                        onChange={this.handleChange}
+                        id="roomName"
+                        style={{ width: "300px", borderRadius: "20px", paddingLeft: "24px" }}
+                    />
+                    <Label style={{ marginTop: "20px" }}>Number of Icebreaker Questions</Label>
                     <InputGroup>
                         <Input
                             placeholder="Maximum is 15"
@@ -97,18 +138,18 @@ export class GameManager extends Component {
                             step="1"
                             defaultValue={5}
                             onChange={this.handleChange}
+                        // style={{width: "20px"}}
                         />
                         <InputGroupAddon addonType="append">Questions</InputGroupAddon>
                     </InputGroup>
-                </div>
-                <FormGroup>
-                    <Label check>
-                        <Input type="checkbox" name="toggleAnalysis" onClick={this.onClick} /> Show Analysis
+                    <Label check style={{ marginTop: "30px", marginLeft: "40px", fontSize: "16px", color: "#226597", fontWeight: "600" }}>
+                        <input type="check box"
+                            name="toggleAnalysis"
+                            onClick={this.onClick}
+                        /> Show Analysis
                     </Label>
-                </FormGroup>
-                <a href="/Categories">
-                    <Button onClick={this.handleCreateRoom} disabled={!isEnabled}>Next</Button>
-                </a>
+                    {button}
+                </Form>
             </div>
         );
     }
@@ -118,101 +159,145 @@ export class Categories extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            category: "",
-            questions:[]
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange = e => {
-        this.setState({
-            category: e.target.name,
             questions: []
-        });
-        this.getQuestions();
+        }
+        this.setQuestionDeck = this.setQuestionDeck.bind(this);
     }
 
-    getQuestions() {
-        fetch('../src/questions.json').then(function (response) {
-            console.log("does it go in the first bit");
-            return response.text();
-        }).then(function(data) {
-            console.log("does it go in the second bit");
-            console.log(data);
-            this.setState({questions:data});
-            return data;
-        }).catch(function(error) {
-            //function to catch the error if the questions is not loaded
+    componentDidMount() {
+        // Check file whether file is empty
+        if (questionFile.length === 0) {
             alert("An error occured while finding the questions, please try again later or contact the owner of the website");
-            console.log(error);
-        })
+        }
+    }
+
+    setQuestionDeck = (e) => {
+        let deck = this.getQuestions(e.target.name);
+        if (deck !== undefined) {
+            this.setState({
+                questions: deck
+            });
+        } else {
+            alert("An error occured while finding the questions, please try again later or contact the owner of the website");
+        }
+    }
+
+    getQuestions(category) {
+        for (let questionSet of questionFile) {
+            if (questionSet.category === category) {
+                return questionSet.questions;
+            }
+        }
+        return undefined;
     }
 
     render() {
+        let isEnabled = (this.state.questions.length !== 0);
         return (
-            <div>
+            <div id="category">
                 <h1>Categories</h1>
                 <Row>
                     <Col>
-                        <div>
-                            <p>Travel</p>
-                            <img onClick={this.handleChange} src={require("./icons/travel.png")} name="travel" alt="travel" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" src={require("./icons/travel.png")} name="travel" alt="travel" />
                         </div>
                     </Col>
                     <Col>
-                        <div>
-                            <p>Food</p>
-                            <img onClick={this.handleChange} src={require("./icons/food.png")} name="food" alt="food" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" src={require("./icons/food.png")} name="food" alt="food" />
                         </div>
                     </Col>
                     <Col>
-                        <div>
-                            <p>Sports</p>
-                            <img onClick={this.handleChange} src={require("./icons/sports.png")} name="sports" alt="sports" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" src={require("./icons/sports.png")} name="sports" alt="sports" />
                         </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <div>
-                            <p>Music</p>
-                            <img onClick={this.handleChange} src={require("./icons/music.png")} name="music" alt="music" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" src={require("./icons/music.png")} name="music" alt="music" />
                         </div>
                     </Col>
                     <Col>
-                        <div>
-                            <p>Movies</p>
-                            <img onClick={this.handleChange} src={require("./icons/movie.png")} name="movie" alt="movie" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" src={require("./icons/movie.png")} name="movie" alt="movie" />
                         </div>
                     </Col>
                     <Col>
-                        <div>
-                            <p>Books</p>
-                            <img onClick={this.handleChange} src={require("./icons/book.png")} name="book" alt="book" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" onClick={this.handleChange} src={require("./icons/book.png")} name="book" alt="book" />
                         </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <div>
-                            <p>Animals</p>
-                            <img onClick={this.handleChange} src={require("./icons/animal.png")} name="animal" alt="animal" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" src={require("./icons/animal.png")} name="animal" alt="animal" />
                         </div>
                     </Col>
                     <Col>
-                        <div>
-                            <p>Random</p>
-                            <img id="random" onClick={this.handleChange} src={require("./icons/random.png")} name="random" alt="random" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" id="random" src={require("./icons/random.png")} name="random" alt="random" />
                         </div>
                     </Col>
                     <Col>
-                        <div>
-                            <p>Customize</p>
-                            <img id="customized" onClick={this.handleChange} src={require("./icons/customized.png")} name="customized" alt="customized" />
+                        <div className="cata" onClick={this.setQuestionDeck}>
+                            <img className="cataimg" id="customized" src={require("./icons/customized.png")} name="customized" alt="customized" />
                         </div>
                     </Col>
                 </Row>
+                <ModalQuestions questionList={this.state.questions} max={this.props.location.state.maxQuestions}/>
+                <Button href="/Room" disabled={isEnabled}>Go to Room</Button>
             </div>
         );
     }
 }
+
+class ModalQuestions extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        modal: false
+      };
+      this.toggle = this.toggle.bind(this);
+    }
+  
+    toggle() {
+      this.setState(prevState => ({
+        modal: !prevState.modal
+      }));
+    }
+
+    render() {
+        if(this.props.questionList.length !== 0) {
+            const entries = Object.entries(this.props.questionList);
+            let displayQuestion = entries[0];
+            let displayQuestionModal = displayQuestion[0];
+            let values = Object.values(this.props.questionList);
+            let displayButton = values[0];
+            let displayButton1 = displayButton[0];
+            let displayButton2 = displayButton[1];
+            return (
+                <div>
+                  <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+                  <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>{displayQuestionModal}</ModalHeader>
+                    <ModalFooter>
+                      <Button color="primary" onClick={this.toggle}>{displayButton1}</Button>{' '}
+                      <Button color="primary" onClick={this.toggle}>{displayButton2}</Button>{' '}
+                      <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                  </Modal>
+                </div>
+              );
+        }
+        return(
+            <div>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <p>Organizer has not posted question yet</p>
+                </Modal>
+            </div>
+        )
+    }
+  }
