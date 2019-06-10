0c7f3313d0ca75f5b7cd61bb752b90cd059e866f
Yiran Ni <yirann@uw.com>
2019-06-11 07:46:53 +0800

    Updates

---
+++ precache-manifest.4e2ac74529d770b5588e8df5e9b36e82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c79b4fb44fb29e4ec07fd0c7e3e0c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bee2db9c18dded3e870",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2742585ea5754f451448",
+    "url": "/static/css/main.221c1ed1.chunk.css"
+  },
+  {
+    "revision": "8bee2db9c18dded3e870",
+    "url": "/static/js/2.1ca70ab2.chunk.js"
+  },
+  {
+    "revision": "2742585ea5754f451448",
+    "url": "/static/js/main.fb9413ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "96f7eb1f8150c130334ee0e9d786cd17",
+    "url": "/static/media/copy.96f7eb1f.svg"
+  },
+  {
+    "revision": "0578a24bcc077bdcd0c34cf70458afc8",
+    "url": "/static/media/profile3.0578a24b.png"
+  },
+  {
+    "revision": "61e8ae66333ea13ecf50a9bed2662e49",
+    "url": "/static/media/setting.61e8ae66.svg"
+  },
+  {
+    "revision": "a22115c19deeedb03b51c7e331c5601e",
+    "url": "/static/media/trash.a22115c1.svg"
+  }
+]);
